e03ab34d32363b4f5193a13a416f5b94fce73c51
Muzz <jarvismr0@gmail.com>
2026-08-21 20:09:51 +0530

    Add commit webhook handler v1.3

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,14 +12,15 @@ import { clerkMiddleware } from "@clerk/express";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
+const app = express();
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-const app = express();
 
 const publicDir = path.join(process.cwd(), "public");
 
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json"}), clerkWebhook)
 app.use(express.json());
 app.use(cors({
     origin: FRONTEND_URL,
@@ -29,7 +30,6 @@ app.use(cors({
 
 app.use(clerkMiddleware());
 
-app.use("/api/webhooks/clerk", express.raw({ type: "application/json"}), clerkWebhook)
 
 app.get("/health", (req,res) => {
     res.status(200).json({ok: true});
