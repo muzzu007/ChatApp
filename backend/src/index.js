facc1e9cf1a881f8d11aac897bd8124c6fcb12d6
Muzz <jarvismr0@gmail.com>
2026-08-22 22:28:16 +0530

    backend Done v1.0

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,55 +5,50 @@ import "dotenv/config";
 
 import fs from "fs";
 import path from "path";
-import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js";
-import authRoutes from "./routes/auth.route.js";
-import authRoutes from "./routes/message.route.js";
-
-import {app, server} from "./lib/socket.js"
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
+import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-
 const publicDir = path.join(process.cwd(), "public");
 
-app.use("/api/webhooks/clerk", express.raw({ type: "application/json"}), clerkWebhook)
+// it's important that you don't parse the webhook event data, it should be in the raw format
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
+
 app.use(express.json());
-app.use(cors({
-    origin: FRONTEND_URL,
-    credentials: true
-}));
-
-
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
 
-
-app.get("/health", (req,res) => {
-    res.status(200).json({ok: true});
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
-if(fs.existsSync(publicDir)){
-    app.use(express.static(publicDir))
-    app.get("/{*any}", (req,res,next) =>{
-        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
 
-    });
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
 }
 
+server.listen(PORT, () => {
+  connectDB();
+  console.log("Server is up and running on PORT:", PORT);
 
-server.listen(PORT, ()=>{
-    connectDB();
-    console.log("Server is up and running on port 3000");
-    if(process.env.NODE_ENV === "production") job.start();
-    
-})
+  if (process.env.NODE_ENV === "production") job.start();
+});
